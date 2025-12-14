2e2e7c5dd2432da0d0165db9643fc772b3893719
Vedant Achtani <72167208+VxDant@users.noreply.github.com>
2025-12-14 23:30:37 +0100

    first commit

---
+++ addition.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func addition(x int, y int) {
+	fmt.Println("code inside addition file")
+
+	fmt.Println(x + y)
+
+}
---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	myVariable := "The String is very important part"
+
+	fmt.Println(myVariable)
+
+	x := 51
+	y := 51
+
+	addition(x, y)
+}
